b832a8dc80d0aa768a3d6d37666b59497147eecb
Muhammad Najeeb <yusnaj21@gmail.com>
2024-04-13 23:05:55 +0100

    Modified: database

--- src/components/PostPage.jsx
+++ src/components/PostPage.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+const API_URL = "http://localhost:3500/items";
 const PostPage = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL);
+        const listItems = await response.json();
+        setItems(listItems);
+      } catch (err) {
+        console.log(err.stack);
+      }
+    };
+
+    fetchItems();
+  }, []);
   return (
-    <div>
-      <h1>Post Page</h1>
-    </div>
+    <>
+      {items.length ? (
+        items.map((item) => {
+          return (
+            <ul key={item.id}>
+              <li>
+                {item.id}. {item.item}
+              </li>
+            </ul>
+          );
+        })
+      ) : (
+        <p>No data found</p>
+      )}
+    </>
   );
 };
 
