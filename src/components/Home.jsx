05941ef449a21784c95537ad8791d15605202ada
Muhammad Najeeb <yusnaj21@gmail.com>
2024-04-14 23:19:29 +0100

    Modified: JSON server

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useRef, useState } from "react";
 import apiRequest from "../apiRequest";
-import { BsPlus } from "react-icons/bs";
+import { BsPlus, BsX, BsPen } from "react-icons/bs";
+const API_URL = "http://localhost:3500/items";
 
 const Home = () => {
-  const API_URL = "http://localhost:3500/items";
   const [items, setItems] = useState([]);
   const [fetchError, setFetchError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
@@ -11,37 +11,41 @@ const Home = () => {
   // Creating a new Item
   const [newItem, setNewItem] = useState("");
   const inputRef = useRef();
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(API_URL);
         if (!response.ok) throw Error("Couldn't get data");
-        const DATA = await response.json();
+        const allItems = await response.json();
 
+        setItems(allItems);
         setFetchError(null);
-        setItems(DATA);
       } catch (err) {
         setFetchError(err.message);
       } finally {
         setIsLoading(false);
       }
     };
-    setInterval(() => {
+
+    setTimeout(() => {
       fetchData();
     }, 2000);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const addItem = async () => {
-    const id = items.length ? items[items.length - 1].id + 1 : 1;
-    const myNewItem = { id, checked: false, newItem };
+    const id = items.length ? +items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item: newItem };
 
     const newListItem = [...items, myNewItem];
     setItems(newListItem);
-    // console.log(newListItem);
     const postOptions = {
       method: "POST",
       header: {
-        "content-type": "application/json",
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(myNewItem),
     };
@@ -56,11 +60,12 @@ const Home = () => {
     );
     setItems(listItems);
 
+    // Updates JSON Server
     const myCheckedItem = listItems.filter((item) => item.id === id);
     const updateOptions = {
       method: "PATCH",
       headers: {
-        "content-type": "application/json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ checked: myCheckedItem[0].checked }),
     };
@@ -68,9 +73,56 @@ const Home = () => {
     const result = await apiRequest(reqUrl, updateOptions);
     if (result) setFetchError(result);
   };
+
+  const handleDelete = async (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+
+    const deleteOptions = {
+      method: "DELETE",
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, deleteOptions);
+
+    if (result) setFetchError(result);
+  };
+  const handleEdit = (id) => {
+    const itemToEdit = items.filter((item) => item.id === id);
+
+    console.log(...itemToEdit);
+    // setNewItem(itemToEdit[0].item);
+
+    const updatedItem = {
+      id,
+      checked: !itemToEdit[0].checked,
+      item: newItem.item,
+    };
+
+    // Patch request
+
+    const updateOptions = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedItem),
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = apiRequest(reqUrl, updateOptions);
+
+    if (result) setFetchError(result);
+  };
+
+  const handleUpdate = () => {
+    console.log("Updated");
+  };
+
   return (
     <>
-      <form onSubmit={(e) => e.preventDefault()}>
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="flex items-center gap-4 my-4"
+      >
         <input
           className="p-1 border border-black"
           type="text"
@@ -80,8 +132,10 @@ const Home = () => {
           placeholder="Add new item..."
           ref={inputRef}
         />
+
         <button
-          className="border p-1 rounded"
+          className="border p-1 rounded bg-blue-500"
+          // eslint-disable-next-line jsx-a11y/aria-role
           role="submit"
           onClick={() => {
             inputRef.current.focus();
@@ -89,12 +143,17 @@ const Home = () => {
             setNewItem("");
           }}
         >
-          <BsPlus size={30} />
+          <BsPlus size={25} />
+        </button>
+        <button
+          className="border py-1 px-3 rounded bg-green-950 text-white"
+          onClick={() => handleUpdate()}
+        >
+          Update
         </button>
       </form>
       <div>
         {isLoading && <p>Loading items...</p>}
-
         {!fetchError &&
           !isLoading &&
           items.map((item, i) => {
@@ -103,10 +162,22 @@ const Home = () => {
                 <form>
                   <input
                     type="checkbox"
-                    onChange={() => handleCheck(item.id)}
+                    onChange={() => {
+                      handleCheck(item.id);
+                    }}
                   />
                 </form>
                 <p>{item.item}</p>
+                <BsPen
+                  size={30}
+                  className="border bg-red-400 p-2 rounded-lg"
+                  onClick={() => handleEdit(item.id)}
+                />
+                <BsX
+                  size={30}
+                  className="border bg-red-400 p-2 rounded-lg"
+                  onClick={() => handleDelete(item.id)}
+                />
               </div>
             );
           })}
--- src/components/PostPage.jsx
+++ src/components/PostPage.jsx
@@ -1,8 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import { BsPlus, BsX } from "react-icons/bs";
 
 const API_URL = "http://localhost:3500/items";
 const PostPage = () => {
   const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState("");
+
+  const inputRef = useRef();
 
   useEffect(() => {
     const fetchItems = async () => {
@@ -17,14 +21,57 @@ const PostPage = () => {
 
     fetchItems();
   }, []);
+  const handleCheck = (id) => {
+    const checkItem = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(checkItem);
+  };
   return (
     <>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          className="p-1 border border-black"
+          type="text"
+          autoFocus
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+          placeholder="Add new item..."
+          ref={inputRef}
+        />
+        <button
+          className="border p-1 rounded"
+          // eslint-disable-next-line jsx-a11y/aria-role
+          role="submit"
+          onClick={() => {
+            inputRef.current.focus();
+            // addItem();
+            // setNewItem("");
+          }}
+        >
+          <BsPlus size={30} />
+        </button>
+      </form>
       {items.length ? (
         items.map((item) => {
           return (
             <ul key={item.id}>
-              <li>
-                {item.id}. {item.item}
+              <li className="flex gap-4 items-center">
+                {item.id}.
+                <input
+                  type="checkbox"
+                  onChange={(e) => {
+                    e.preventDefault();
+                    handleCheck(item.id);
+                  }}
+                />
+                <p>{item.item}</p>
+                <BsX
+                  // onClick={() => handleUpdate(item.id)}
+                  aria-label="button"
+                  size={20}
+                  className="border bg-red-800 rounded text-white cursor-pointer"
+                />
               </li>
             </ul>
           );
